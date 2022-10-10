339e9668c827d379c57c576e725eee22ef3bbb64
Bhavik Glibert <83555685+Bhavik-Gilbert@users.noreply.github.com>
2022-10-10 17:40:45 +0100

    Add signup form
    Created signup page and added signup form

--- microblogs/views.py
+++ microblogs/views.py
@@ -1,6 +1,11 @@
-from urllib import response
 from django.shortcuts import render
+
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def signup(request):
+    form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
